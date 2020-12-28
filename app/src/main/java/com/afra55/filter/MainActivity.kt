37efe7707adc5448fdc54f7bbb49720aa8faf305
afra55 <afra55@foxmail.com>
2020-12-28 16:28:19 +0800

    choose a bitmap

--- app/src/main/java/com/afra55/filter/MainActivity.kt
+++ app/src/main/java/com/afra55/filter/MainActivity.kt
@@ -1,16 +1,79 @@
 package com.afra55.filter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    var photoBitmap:Bitmap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Example of a call to a native method
-        findViewById<TextView>(R.id.sample_text).text = stringFromJNI()
+        sample_text.text = stringFromJNI()
+
+        checkPermission()
+
+        sample_text.setOnClickListener {
+            if (checkPermission()) {
+
+                val contentSelectionIntent = Intent(Intent.ACTION_GET_CONTENT)
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE)
+                contentSelectionIntent.type = "image/*"
+                val chooserIntent = Intent(Intent.ACTION_CHOOSER)
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent)
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser")
+                startActivityForResult(chooserIntent, 1)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            val photo = data?.dataString
+            if (photo != null){
+                Glide.with(this).load(photo).into(sample_image)
+                Glide.with(this).asBitmap().load(photo).into(object : CustomTarget<Bitmap>() {
+                    override fun onLoadCleared(placeholder: Drawable?) {
+
+                    }
+
+                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                        photoBitmap = resource
+                    }
+
+                })
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (photoBitmap?.isRecycled != true) {
+            photoBitmap?.recycle()
+        }
+    }
+
+    private fun checkPermission():Boolean {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1111)
+            return false
+        }
+        return true
     }
 
     /**
